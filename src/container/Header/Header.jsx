86b10491c79a0f97e38de0ddbdff7743a15b52b6
amit-maurya <amit_maurya@hcl.com>
2023-07-05 12:37:48 +0530

    changes-in-CSS

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div className="app__wrapper_info-Header">
       <SubHeading title="OUR VISION" />
       <SubHeading title="SUPERIORITY OF HUMAN LIFE" />
-      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+      <p className="p__opensans-header" style={{ margin: "2rem 0" }}>
       Shanti Nandan Bauddha Welfare Society strongly believes in the philosophy of Gautam Buddha, because Buddhism is not a Religion, it is a way of giving dignity to life, an actual path that leads to the alleviation of suffering.
       The purpose of formation of Shanti Nandan Bauddha Welfare Society is to execute the philosophy of Gautam Buddha into reality, to reduce human suffering and improve quality of life through relevant education, innovative healthcare and market-focused livelihood program
       </p>
@@ -23,7 +23,7 @@ const Header = () => (
       </div>
     </div>
 
-    <div className="app__wrapper_img">
+    <div className="app__wrapper_img-Header">
       <img src={images.people} alt="header img" />
     </div>
   </div>
